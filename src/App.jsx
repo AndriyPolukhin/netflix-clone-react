cb75ad202f597cf9b048622dc6c0f4c4806f5976
AndriyPolukhin <andriy.polukhin@gmail.com>
2020-08-06 08:22:59 +0300

    axios setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <h1>Let's build Netflix Clone Front-End</h1>
     </div>
   );
-}
+};
 
 export default App;
